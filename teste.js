04256402a71fb752135723f467443cb0e7079c83
Andrei <andrei09ka6@gmail.com>
2021-07-17 23:36:08 -0300

    Hiring Coders

---
+++ teste.js
@@ -0,0 +1,23 @@
+
+document.getElementById("button").addEventListener("click", 
+
+function(event){
+    event.preventDefault()
+    alert("Usuário salvo no localStorage")
+
+    let salvarNome = document.getElementById("nome")
+    let salvarEmail = document.getElementById("email")
+
+    const handleInputNome = salvarNome.value;
+    const handleInputEmail = salvarEmail.value;
+
+    localStorage.setItem("Nome", handleInputNome)
+    localStorage.setItem("Email", handleInputEmail)
+           
+            console.log("entrou")
+})
+        
+    
+
+  
+   
